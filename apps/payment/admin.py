e257c24a3f88a71c09afcfd799380b3b1b218f6f
Arsenchique <zhanshin2005@gmail.com>
2025-10-20 14:59:50 +0500

    Added docstrings and typing

--- apps/payment/admin.py
+++ apps/payment/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
 from .models import Payment
 
-admin.site.register(Payment)
+
+class PaymentAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Payment model.
+
+    This class defines how Payment objects are displayed and managed
+    in the Django admin interface.
+    """
+    list_display: tuple[str, ...] = ("user", "amount", "payment_method", "status", "created_at")
+    list_filter: tuple[str, ...] = ("status", "payment_method")
+    search_fields: tuple[str, ...] = ("user__username",)
+    ordering: tuple[str, ...] = ("-created_at",)
+
+
+# Register the Payment model in the admin site
+admin.site.register(Payment, PaymentAdmin)
+
--- apps/payment/apps.py
+++ apps/payment/apps.py
@@ -1,5 +1,13 @@
 from django.apps import AppConfig
 
+
 class PaymentConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.payment'
+    """
+    Configuration class for the Payment application.
+
+    This class defines metadata for the 'apps.payment' app,
+    such as its name and default settings for auto-created primary keys.
+    """
+
+    default_auto_field: str = 'django.db.models.BigAutoField'
+    name: str = 'apps.payment'
